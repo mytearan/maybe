6d387c52aacf3247da6bafaed3f7422e16a98d5a
mytearan <51380012+mytearan@users.noreply.github.com>
2021-05-20 19:46:58 -0700

    Update About.vue
    added extra text to the about.vue. couldn't get images to work

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 style="text-align:center">About HWERTZ</h1>
+    <p>
+      We are creating a Car Rental Web Application that could serve a small to medium sized business with a online store front.
+As we transition through the epidemic, consumers may not feel as comfortable as they used to. Having a method that reduces the time spent in a physical location may contribute to a consumers decision to use one rental service over another.
+Also digitizing all the paperwork involved in the car rental process would make backups and searches more efficient.
+    </p>
+    <h2 style="text-align:center">Why We're Here</h2>
+    <p>HWERTZ would allow suppliers to upload their fleet onto our application. We would let suppliers see who has what car for how many days. HWERTZ would also let suppliers see how many cars are left in their fleet.
+HWERTZ would allow customers to register. This would include name address credit, debit, or bank account number; all of the information needed to begin the car rental process. Due to our limited team number we would limit HWERTZ customers to one supplier. </p>
+    <h4 style="text-align:center">Disclaimer</h4>
+  <p> HWERTZ is limited to booking a car from a fleet. Given our limited team number we will have to exclude digital signatures and contracts (legal or Insurance) from HWERTZ. </p>
   </div>
 </template>
